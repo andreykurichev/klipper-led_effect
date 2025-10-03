9736739fdcfcc46477313a1ec635479b9636122a
andreykurichev <25134138+andreykurichev@users.noreply.github.com>
2025-10-03 23:11:42 +0300

    Add files via upload

--- install-led_effect.sh
+++ install-led_effect.sh
@@ -70,24 +70,6 @@ restart_moonraker()
     echo "[OK]"
 }
 
-# Add updater for led_effect to moonraker.conf
-add_updater()
-{
-    echo -e -n "Adding update manager to moonraker.conf... "
-
-    update_section=$(grep -c '\[update_manager led_effect.py\]' ${MOONRAKER_CONFIG_DIR}/moonraker.conf || true)
-    if [ "${update_section}" -eq 0 ]; then
-        echo -e "\n" >> ${MOONRAKER_CONFIG_DIR}/moonraker.conf
-        while read -r line; do
-            echo -e "${line}" >> ${MOONRAKER_CONFIG_DIR}/moonraker.conf
-        done < "$PWD/file_templates/moonraker_update.txt"
-        echo -e "\n" >> ${MOONRAKER_CONFIG_DIR}/moonraker.conf
-        echo "[OK]"
-        restart_moonraker
-        else
-        echo -e "[update_manager led_effect.py] already exists in moonraker.conf [SKIPPED]"
-    fi
-}
 
 restart_klipper()
 {
